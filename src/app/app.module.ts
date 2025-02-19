2befd5a279e8ecb9147310dc7b79ad744b6c9c39
natchanan-p <natchanan.pathmarit@gmail.com>
2025-02-19 21:57:32 +0700

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import {AppComponent} from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+
+export class AppModule {}
